49a60ce27db4acfbe214f918ec2bbf313f7ee190
bashar <muhtasim2k2@gmail.com>
2025-05-31 18:00:54 +0300

    Added metadata and initialized contact me

--- Portfolio/muhtasims-portfolio/app/page.jsx
+++ Portfolio/muhtasims-portfolio/app/page.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import ContactMe from "./components/ContactMe";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       {/* <About/> */}
       <Projects/>
       <Skills/>
+      <ContactMe/>
     </>
   );
 }
